74f6d79b68e54c1754fe05d7611808056f8c0751
Olusegun M. Isaac <oluisaac7@gmail.com>
2019-03-07 10:47:04 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,21 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var element = document.createElement('div'); {
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = '#f9f9f9';
+}
+var element.appendChild();{
+  document.body.appendChild(element);
+}
+var element.textAlign(){
+  element.style.textAlign = 'center';
+}
+var ul = document.createElement('ul');{
+  ul.style.textAlign = 'left';
+}
+var element.removeChild();{
+  ul.removeChild(ul.querySelector('li:nth-child(2)'));
+}
+var element.remove();{
+  ul.remove();
+}
